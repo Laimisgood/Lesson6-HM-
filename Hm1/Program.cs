37873ee6fd075256b90bb299e5af29835b850c18
Sardor <sardor.bobojonov@gmail.com>
2022-10-25 14:34:35 +0500

    initial commit

---
+++ Hm1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Разделить ввод данных, от расчета данных (Одна функция вводит данные, другая функция подсчитывает количество положительных)
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3 // Тут правильно должно быть 4, возможно просто опечатка
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreateArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        System.Console.Write("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("Массив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int PosNums(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if(array[i]>0)
+       {
+            count += 1;
+       } 
+    }
+    return count;
+}
+
+int value = Prompt("Сколько чисел хотите ввести? = ");
+int[] array = CreateArray(value);
+int count = PosNums(array);
+System.Console.WriteLine();
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine($"Количество чисел больше нуля = {count}");
---
+++ Hm2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+double Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double k1 = Prompt("Введите значение k1 = ");
+double k2 = Prompt("Введите значение k2 = ");
+double b1 = Prompt("Введите значение b1 = ");
+double b2 = Prompt("Введите значение b2 = ");
+
+void Condition(double k1, double k2, double b1, double b2)
+{
+    double x, y;
+    if ((k1 == k2) && (b1 == b2))
+    {
+        System.Console.WriteLine("Прямые совпадают");
+    }
+
+    else if (k1 == k2)
+    {
+        System.Console.WriteLine("Прямые параллельны");
+    }
+
+    else
+    {
+
+        x = (b2 - b1) / (k1 - k2);
+
+        y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+
+        System.Console.WriteLine($"({x}, {y})");
+
+    }
+}
+
+Condition(k1, k2, b1, b2);
